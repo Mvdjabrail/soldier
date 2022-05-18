53e2c9673abbb1aa197c0a86e0d401bd4ea3c9af
Djabrail Magamadov <magamadov.ru095@gmail.com>
2022-05-18 11:40:44 +0300

    Доработал методы

--- soldat.js
+++ soldat.js
@@ -1,15 +1,40 @@
 const soldier = {
-    soldat: {
-        soldatName: 'Emin',
-        health: 10,
-        supplies: 3,
-        weapon: {
-            nameWeapon: 'Calash',
-            numberOfRounds: '30',
-        },
+    soldatName: 'Emin',
+    health: 10,
+    supplies: 3,
+    weapon: {
+        nameWeapon: 'Calash',
+        numberOfRounds: 30,
+
     },
-    fire() { },
-    reload() { },
-    hurt() { },
+    fire() {
+        if (this.weapon.numberOfRounds !== 0
+        ) {
+            this.weapon.numberOfRounds--
+            console.log('Ба-бах')
+        }
+        else {
+            console.log('обойма пуста. Перезаредитесь')
+        }
+    },
+    reload() {
+        if (this.weapon.numberOfRounds < 30 && this.supplies > 0) {
+            this.weapon.numberOfRounds = 30
+            this.supplies--
+            console.log('перезарядка...')
+        } else if (this.supplies <= 0) {
+                console.log('не осталось припасов')
+            }
+            
+
+    },
+    hurt() { 
+        if(this.health === 0){
+            console.log('Ты умер')
+        }
+        else if(this.health--){
+            console.log('Ты ранен')
+        }
+    },
 
 }
