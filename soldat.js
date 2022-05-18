48f84655a4b3136153d131138170dfccc721b4e1
Djabrail Magamadov <magamadov.ru095@gmail.com>
2022-05-18 10:44:25 +0300

    Добавил объект soldier

---
+++ soldat.js
@@ -0,0 +1,15 @@
+const soldier = {
+    soldat: {
+        soldatName: 'Emin',
+        health: 10,
+        supplies: 3,
+        weapon: {
+            nameWeapon: 'Calash',
+            numberOfRounds: '30',
+        },
+    },
+    fire() { },
+    reload() { },
+    hurt() { },
+
+}
